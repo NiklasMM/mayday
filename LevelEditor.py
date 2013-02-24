81c734eacd856fb63bfbe1ed7f1e95417d12a015
Nikolaus Mayer <nikolaus.mayer@merkur.uni-freiburg.de>
2013-02-24 12:08:04 +0100

    Debug message printing now toggle-able (CPU intensive)
    New HelixArc object type
    
    Fixed single-click selection bug

--- LevelEditor.py
+++ LevelEditor.py
@@ -7,12 +7,14 @@
 
 ########################################################################
 # TODO
-# - mouse camera ?
-# - clickable game objects / how precise?
+# - mouse camera DONE
+# - clickable game objects / how precise? DONE
 # - z-ordering (treap?)
-# - adding / moving objects
+# - adding DONE / moving objects
 # - autojoining objects at ends to form a path
+# - context menu
 #
+# ONLY IN GAME
 # - wind (+ graphic effects?)
 # - cosmetic effects: rain, snow (particles?)
 # - windfarms, birds, cars on highways
@@ -22,6 +24,7 @@
 ########################################################################
 
 import pygame
+import pygame.gfxdraw  # aaellipse
 from math import pi, sin, cos
 import logging, sys, os
 from collections import deque
@@ -36,14 +39,14 @@ ZOOM_IN_SPEED = 1.05
 ZOOM_OUT_SPEED = 0.95
 MOUSE_ROTATION_ANGULAR_SPEED = 0.1
 MOUSE_ELEVATION_ANGULAR_SPEED = 0.1
-MOUSE_ZOOM_IN_SPEED = 0.5
-MOUSE_ZOOM_OUT_SPEED = 0.5
+MOUSE_ZOOM_IN_SPEED = 0.1
+MOUSE_ZOOM_OUT_SPEED = 0.1
 
 # Don't have to hold the mouse perfectly still for "clicks" (vs dragging)
 DRAGGING_DISTANCE_THRESHOLD = 5
 
-rotation = -45*(pi/180.)
-elevation = -66*(pi/180.)
+rotation = -45.*(pi/180.)
+elevation = -66.*(pi/180.)
 right = [cos(rotation), sin(rotation) * cos(elevation)]
 front = [sin(rotation), -cos(rotation) * cos(elevation)]
 up = [0, sin(elevation)]
@@ -57,24 +60,35 @@ messageQueue = deque()
 idleClick = True
 
 
-tooltip_texts = {"AddStraightButton": "add a straight path piece",
-                 "AppendStraightButton": "append a straight path piece to a selected path piece"}
+tooltip_texts = {"AddStraightButton": "Add a straight path piece",
+                 "AppendStraightButton": "Append a straight path piece to a selected path piece"}
 
+
+#_______________________________________________________________________
 
 class displayedObject(object):
   def __init__(self):
     self.surfaceObj = None
     self.rect = None
     self.selected = False
+    # Some objects have (unique!) names
+    self.name = ""
 
   def select(self):
     self.selected = True
-
   def deselect(self):
     self.selected = False
 
   def render(self):
     return None
+
+  def draw(self, screen):
+    """
+    Draw to screen
+    IMPORTANT: Call self.render() before if the object's appearance depends
+               on the camera pose!
+    """
+    screen.blit(self.surfaceObj, self.rect)
 
 
 class clickRegisteringObject(displayedObject):
@@ -99,35 +113,51 @@ class Button(clickRegisteringObject):
                buttonClickedSurfaceObj=pygame.Surface((0,0)),
                buttonHighlightedSurfaceObj=pygame.Surface((0,0))):
     self.name = name
-    self.surfaceObj = buttonSurfaceObj
+    # .convert() enables surface alpha on PNG images (for "button disabled" transparency)
+    self.surfaceObj = buttonSurfaceObj.convert()
     self.clickedSurfaceObj = buttonClickedSurfaceObj
     self.highlightedSurfaceObj = buttonHighlightedSurfaceObj
     self.rect = buttonRect
+    # Button function activated
     self.active = False
+    # Cursor on button
     self.highlighted = False
+    # Button is clickable
+    self.enabled = True
+    # TODO call super.constructor?
 
   def clickAction(self):
     global idleClick
     idleClick = False
-    return None
+    if not self.enabled:
+      infoMessage('This button is not enabled')
+      raise Exception('Button %s is not enabled' % self.name)
 
   def setRectangle(self, newRectangle):
     self.rect = newRectangle
 
   def activate(self):
-    self.active = True
-
+    if self.enabled:
+      self.active = True
   def deactivate(self):
     self.active = False
 
   def highlight(self):
     self.highlighted = True
-
   def dehighlight(self):
     self.highlighted = False
 
+  def enable(self):
+    self.enabled = True
+    self.surfaceObj.set_alpha(255)
+  def disable(self):
+    self.enabled = False
+    self.surfaceObj.set_alpha(50)
+
   def draw(self, screen):
-    if self.active:
+    if not self.enabled:
+      screen.blit(self.surfaceObj, self.rect)
+    elif self.active:
       screen.blit(self.clickedSurfaceObj, self.rect)
     elif self.highlighted:
       screen.blit(self.highlightedSurfaceObj, self.rect)
@@ -158,10 +188,13 @@ class AddStraightButton(Button):
                                             highlightedImg)
 
   def clickAction(self):
+    try:
+      super(AddStraightButton, self).clickAction()
+    except:
+      return None
     global objectsList
     objectsList.append(Straight((-20,-20,-20),(20,50,20)))
     infoMessage("Straight object added.")
-    super(AddStraightButton, self).clickAction()
 
   def tooltip(self, screen, mousePos=None):
     if mousePos is None:
@@ -184,13 +217,16 @@ class AppendStraightButton(Button):
                                                highlightedImg)
 
   def clickAction(self):
+    try:
+      super(AppendStraightButton, self).clickAction()
+    except:
+      return None
     if not selectedObjects:
       infoMessage("must select a single path piece to append (none selected)")
     elif len(selectedObjects) > 1:
       infoMessage("must select a single path piece to append (%d selected)" % len(selectedObjects))
     else:
       infoMessage("I am not doing anything =(")
-    super(AppendStraightButton, self).clickAction()
 
   def tooltip(self, screen, mousePos=None):
     if mousePos is None:
@@ -199,10 +235,12 @@ class AppendStraightButton(Button):
 
 
 class Straight(clickRegisteringObject):
-  def __init__(self, startPoint3D=(0,0,0), endPoint3D=(0,0,0)):
+  def __init__(self,
+               startPoint3D=(0,0,0), endPoint3D=(0,0,0), color=(0,0,255)):
     super(Straight, self).__init__()
     self.startPoint = startPoint3D
     self.endPoint = endPoint3D
+    self.color = color
     self.render()
 
   def render(self):
@@ -223,7 +261,7 @@ class Straight(clickRegisteringObject):
     tempSurfaceObj = tempSurfaceObj.convert_alpha()
     tempSurfaceObjCenter = (tempSurfaceObj.get_size()[0]//2,
                             tempSurfaceObj.get_size()[1]//2)
-    pygame.draw.aaline(tempSurfaceObj, (0, 0, 255),
+    pygame.draw.aaline(tempSurfaceObj, self.color,
                        (positions[0][0]-linecenter[0]+tempSurfaceObjCenter[0],
                         positions[0][1]-linecenter[1]+tempSurfaceObjCenter[1]),
                        (positions[1][0]-linecenter[0]+tempSurfaceObjCenter[0],
@@ -237,9 +275,110 @@ class Straight(clickRegisteringObject):
     self.surfaceObj = tempSurfaceObj
     self.rect = tempSurfaceObjRect
 
-  def draw(self, screen):
-    """draw to screen (call .render() before if necessary)"""
-    screen.blit(self.surfaceObj, self.rect)
+
+
+class HelixArc(clickRegisteringObject):
+  def __init__(self,
+               startHeight=-40., endHeight=40.,
+               startAngle=0., endAngle=720.,
+               radius=50., centerpoint=(0,0,0),
+               rightHanded=True, color=(0,0,255)):
+    super(HelixArc, self).__init__()
+    self.color = color
+    steps = 1000
+    heightstep = (endHeight-startHeight)/steps
+    stepsize = (endAngle-startAngle)/steps
+    height, angle = startHeight, startAngle
+    self.points3d = []
+    self.points3dHD = []
+    step = 0
+    while angle < endAngle:
+      a = angle if rightHanded else (360.-angle)
+      x = cos(a*(pi/180.))*radius + centerpoint[0]
+      y = sin(a*(pi/180.))*radius + centerpoint[1]
+      z = height + centerpoint[2]
+      self.points3dHD.append((x,y,z))
+      if step % 10 == 0:
+        self.points3d.append((x,y,z))
+      angle += stepsize
+      height += heightstep
+      step += 1
+    self.render()
+
+  def render(self, highdefinition=False):
+    """
+    If highdefinition is FALSE, the HelixArc will be rendered using 100 sample
+    points. If highdefinition is TRUE, 1000 points will be used instead.
+    """
+    minx, miny, maxx, maxy = 9999.,9999.,-9999.,-9999.
+    points = self.points3dHD if highdefinition else self.points3d
+    pixels = []
+    for p in points:
+      px, py = pixelpos(p, (0,0))
+      pixels.append((px,py))
+      minx=min(minx,px)
+      miny=min(miny,py)
+      maxx=max(maxx,px)
+      maxy=max(maxy,py)
+    centershift = (maxx+minx)/2,(maxy+miny)/2
+    sf = pygame.Surface((maxx-minx+4,maxy-miny+4))
+    sf = sf.convert_alpha()
+    sf.fill((0,0,0,0))
+    sfsize=sf.get_size()
+
+    # Draw sample points, using Wu-style antialiasing
+    # NOTE that AA is performed using only the alpha channel!
+    for p in pixels:
+      xint, xfrac = divmod(p[0], 1)
+      yint, yfrac = divmod(p[1], 1)
+
+      if 0 <= int(xint)-int(minx)+2 < WINDOW_SIZE[0] and \
+         0 <= int(yint)-int(miny)+2 < WINDOW_SIZE[1]:
+        c = sf.get_at((int(xint)-int(minx)+2,int(yint)-int(miny)+2))
+        c.r, c.g, c.b = self.color
+        c.a=max(c.a,int(255*(1.-xfrac)*(1.-yfrac)))
+        sf.set_at((int(xint)-int(minx)+2,int(yint)-int(miny)+2),c)
+
+      if 0 <= int(xint)+1-int(minx)+2 < WINDOW_SIZE[0] and \
+         0 <= int(yint)-int(miny)+2 < WINDOW_SIZE[1]:
+        c = sf.get_at((int(xint)+1-int(minx)+2,int(yint)-int(miny)+2))
+        c.r, c.g, c.b = self.color
+        c.a=max(c.a,int(255*(xfrac)*(1.-yfrac)))
+        sf.set_at((int(xint)+1-int(minx)+2,int(yint)-int(miny)+2),c)
+
+      if 0 <= int(xint)-int(minx)+2 < WINDOW_SIZE[0] and \
+         0 <= int(yint)+1-int(miny)+2 < WINDOW_SIZE[1]:
+        c = sf.get_at((int(xint)-int(minx)+2,int(yint)+1-int(miny)+2))
+        c.r, c.g, c.b = self.color
+        c.a=max(c.a,int(255*(1.-xfrac)*(yfrac)))
+        sf.set_at((int(xint)-int(minx)+2,int(yint)+1-int(miny)+2),c)
+
+      if 0 <= int(xint)+1-int(minx)+2 < WINDOW_SIZE[0] and \
+         0 <= int(yint)+1-int(miny)+2 < WINDOW_SIZE[1]:
+        c = sf.get_at((int(xint)+1-int(minx)+2,int(yint)+1-int(miny)+2))
+        c.r, c.g, c.b = self.color
+        c.a=max(c.a,int(255*(xfrac)*(yfrac)))
+        sf.set_at((int(xint)+1-int(minx)+2,int(yint)+1-int(miny)+2),c)
+
+    r = sf.get_rect()
+    r.center=(ORIGIN[0]+centershift[0], ORIGIN[1]+centershift[1])
+    self.surfaceObj = sf
+    self.rect = r
+
+
+#_______________________________________________________________________
+
+
+def getObjectByName(name):
+  "identify objects having unique names"
+  global objectsList
+  result = [o for o in objectsList if o.name==name]
+  if not result:
+    raise IndexError('Objectslist contains no object named "%s"!' % name)
+  elif len(result) > 1:
+    raise IndexError('Objectslist contains multiple objects named "%s"!' % name)
+  else:
+    return result[0]
 
 
 def compute_projection_parameters(newrotation, newelevation, newzoom):
@@ -322,20 +461,25 @@ def render_background(screen):
 
 
 
-def makeButtons():
+def makeGUIButtons():
   """initialize GUI buttons, returns a list of BUTTONs"""
   buttons = []
-  to_make = ((AddStraightButton, "addStraight"),
-             (AppendStraightButton, "appendStraight"))
-  x, y = WINDOW_SIZE[0], 0
+  to_make = ((AddStraightButton, "addStraightButton",
+                (WINDOW_SIZE[0], 0)),
+             (AppendStraightButton, "appendStraightButton",
+                (WINDOW_SIZE[0]-50, 0)))
 
-  for buttonClass, name in to_make:
+  for buttonClass, name, (x,y) in to_make:
     newButton = buttonClass(name)
     rect = newButton.surfaceObj.get_rect()
-    rect.topright = (x, y)
+    rect.topright = (x,y)
     newButton.setRectangle(rect)
     buttons.append(newButton)
-    y += 50
+
+  global objectsList
+  objectsList.extend(buttons)
+
+  getObjectByName("appendStraightButton").disable()
 
   return buttons
 
@@ -407,9 +551,9 @@ def drawHelpDebugInfoMessages(screen, cursor_position, cursorPixelPos):
   for i in range(3):
     textObj = pygame.font.SysFont(None, 18).render(lines[i], True, (0, 0, 0))
     textRect = textObj.get_rect()
-    textRect.topleft = (0, i*15)
+    textRect.topleft = (0, (i+1)*15)
     screen.blit(textObj, textRect)
-  lines = ["Use WASD or MIDDLE MOUSE BUTTON to rotate the camera (isometric projection, camera is at window center).",
+  lines = ["Use WASD or MIDDLE MOUSE BUTTON to rotate the camera (isometric projection).",
            "Move the cursor with the ARROW KEYS and PAGE-UP/DOWN.",
            "Zoom in and out using the + (plus) and - (minus) keys or RIGHT MOUSE BUTTON.",
            "Press HOME to reset the camera."]
@@ -451,9 +595,11 @@ def markObject(obj, screen):
   screen.blit(marker, obj.rect)
 
 
+#_______________________________________________________________________
+
 
 def main():
-  global idleClick
+  global idleClick, objectsList, selectedObjects
 
   logging.basicConfig(level=logging.DEBUG,
                       format='%(asctime)s %(levelname)s: %(message)s',
@@ -483,16 +629,24 @@ def main():
   CursorSurfaceObj.fill((0, 0, 0))
 
   # create GUI buttons
-  objectsList.extend(makeButtons())
+  makeGUIButtons()
+
   boxSelectionInProgress = False
   boxStartPoint = (0, 0)
 
   # How far the mouse has travelled with a button down, used to distinguish
   # between "click" and "drag" actions
   dragManhattanDistance = 0
+  # Prevents "slipping" off GUI buttons into dragging mode
+  dragStartedOnGUI = False
   # Mouse status saved from previous timetick
   lmbLastTick = mmbLastTick = rmbLastTick = False
 
+  # Print info and debugging text?
+  printDebug = False
+
+  ### DEBUG
+  objectsList.append(HelixArc())
 
   # MAIN LOOP
   running = True
@@ -517,8 +671,10 @@ def main():
     elif lmbDown or mmbDown or rmbDown:
       dragManhattanDistance = 0
 
-    if lmbDown and dragManhattanDistance > DRAGGING_DISTANCE_THRESHOLD and \
-      not boxSelectionInProgress:
+    if lmbDown and \
+       dragManhattanDistance > DRAGGING_DISTANCE_THRESHOLD and \
+       not boxSelectionInProgress and \
+       not dragStartedOnGUI:
       deselectObjects()
       boxSelectionInProgress = True
       boxStartPoint = pygame.mouse.get_pos()
@@ -545,69 +701,78 @@ def main():
           for o in objectsList:
             if isinstance(o, Button) and o.cursorOnObject():
               GUIwasClicked = True
+              dragStartedOnGUI = True
               o.activate()
               o.clickAction()
-          # if not GUIwasClicked:
-            # deselectObjects()
-            # boxSelectionInProgress = True
-            # boxStartPoint = pygame.mouse.get_pos()
       # Button up
       elif event.type == pygame.MOUSEBUTTONUP:
         # Only "click"-select objects (box selection is done later)
         if not boxSelectionInProgress:
+          deselectObjects()
           for o in objectsList:
             if o.cursorOnObject() and not isinstance(o, Button):
               selectObjects(o)
               infoMessage("Object selected (via Click).")
-            elif isinstance(o, Button):
+              # Click-selection can only select one object at a time
+              # TODO ordering/preference?
+              break
+          for o in objectsList:
+            if isinstance(o, Button):
               o.deactivate()
-        if idleClick:
-          infoMessage("Idle click, deselecting...")
+        # Clicking into nothing is common for "cancel everything"
+        if idleClick and lmbLastTick:
+          infoMessage("Idle lMB click, deselecting all...")
           deselectObjects()
         # Reset drag distance
         dragManhattanDistance = 0
         boxSelectionInProgress = False
+        dragStartedOnGUI = False
 
     # Check current status of keyboard keys
-    pressed_keys = pygame.key.get_pressed()
+    pressedKeys = pygame.key.get_pressed()
     rerender = False
 
+    if pressedKeys[pygame.K_h] and not pressedKeysLastTick[pygame.K_h]:
+      printDebug = not printDebug
+
     # Move cursor
-    if pressed_keys[pygame.K_UP]:
+    if pressedKeys[pygame.K_UP]:
       cursor_position[1] += 10
-    if pressed_keys[pygame.K_DOWN]:
+    if pressedKeys[pygame.K_DOWN]:
       cursor_position[1] -= 10
-    if pressed_keys[pygame.K_LEFT]:
+    if pressedKeys[pygame.K_LEFT]:
       cursor_position[0] -= 10
-    if pressed_keys[pygame.K_RIGHT]:
+    if pressedKeys[pygame.K_RIGHT]:
       cursor_position[0] += 10
-    if pressed_keys[pygame.K_PAGEUP]:
+    if pressedKeys[pygame.K_PAGEUP]:
       cursor_position[2] += 10
-    if pressed_keys[pygame.K_PAGEDOWN]:
+    if pressedKeys[pygame.K_PAGEDOWN]:
       cursor_position[2] -= 10
 
     # Change camera settings using keyboard
-    if pressed_keys[pygame.K_a]:
+    if pressedKeys[pygame.K_a]:
       compute_projection_parameters(rotation-ROTATION_ANGULAR_SPEED, elevation, zoom)
       rerender = True
-    if pressed_keys[pygame.K_d]:
+    if pressedKeys[pygame.K_d]:
       compute_projection_parameters(rotation+ROTATION_ANGULAR_SPEED, elevation, zoom)
       rerender = True
-    if pressed_keys[pygame.K_w]:
+    if pressedKeys[pygame.K_w]:
       compute_projection_parameters(rotation, elevation+ELEVATION_ANGULAR_SPEED, zoom)
       rerender = True
-    if pressed_keys[pygame.K_s]:
+    if pressedKeys[pygame.K_s]:
       compute_projection_parameters(rotation, elevation-ELEVATION_ANGULAR_SPEED, zoom)
       rerender = True
-    if pressed_keys[pygame.K_HOME]:
+    if pressedKeys[pygame.K_HOME]:
       compute_projection_parameters(-45*(pi/180.), -66*(pi/180.), 1.0)
       rerender = True
-    if pressed_keys[pygame.K_MINUS]:
+    if pressedKeys[pygame.K_MINUS]:
       compute_projection_parameters(rotation, elevation, zoom*ZOOM_OUT_SPEED)
       rerender = True
-    if pressed_keys[pygame.K_PLUS]:
+    if pressedKeys[pygame.K_PLUS]:
       compute_projection_parameters(rotation, elevation, zoom*ZOOM_IN_SPEED)
       rerender = True
+
+    pressedKeysLastTick = pressedKeys
 
     ## Change camera settings using mouse
     # Rotation and Elevation angles
@@ -650,8 +815,15 @@ def main():
     cursorPixelPos = pixelpos(cursor_position, ORIGIN)
     screen.blit(CursorSurfaceObj, cursorPixelPos)
 
-    # Print helpful information:
-    drawHelpDebugInfoMessages(screen, cursor_position, cursorPixelPos)
+    # Print helpful information and debugging messages (CPU intensive!)
+    text = 'Press H to toggle debug information.'
+    textObj = pygame.font.SysFont(None, 18).render(text, True, (0, 0, 0))
+    textRect = textObj.get_rect()
+    textRect.topleft = (0,0)
+    screen.blit(textObj, textRect)
+    if printDebug:
+      drawHelpDebugInfoMessages(screen, cursor_position, cursorPixelPos)
+
 
     # Draw lines to visualize the cursor's position in 3D space
     # TODO we don't need a cursor, use this to mark a single selected object?
@@ -689,20 +861,32 @@ def main():
     for o in selectedObjects:
       markObject(o, screen)
 
+    # Update buttons
+    # TODO refactor into Button method?
+    if not selectedObjects or \
+       len([o for o in selectedObjects if isinstance(o, Straight)]) != 1:
+      getObjectByName('appendStraightButton').disable()
+    else:
+      getObjectByName('appendStraightButton').enable()
+
     # Draw GUI buttons
     for o in objectsList:
       if isinstance(o, Button):
-        if o.cursorOnObject() and not boxSelectionInProgress:
+        if o.cursorOnObject() and \
+           not dragManhattanDistance > DRAGGING_DISTANCE_THRESHOLD :
           o.highlight()
           o.draw(screen)
           o.dehighlight()
         else:
           o.draw(screen)
 
-    # Draw GUI tooltips (after drawing all buttons so tooltips are always on top
+    # Draw GUI tooltips (after drawing all buttons -> tooltips always on top)
     for o in objectsList:
-      if isinstance(o, Button) and o.cursorOnObject() and not boxSelectionInProgress:
+      if isinstance(o, Button) and \
+         o.cursorOnObject() and \
+         not boxSelectionInProgress:
         o.tooltip(screen)
+
 
     # Actually draw the stuff to screen
     pygame.display.flip()
@@ -713,6 +897,9 @@ def main():
 
   # Main loop has been exited; game is ending
   logging.info("Goodbye!")
+
+
+#_______________________________________________________________________
 
 
 if __name__ == '__main__':
